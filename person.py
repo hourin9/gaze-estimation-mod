07127fd997004b7dc7227c95abab8e869066ff4a
hourin <phucjaki@gmail.com>
2025-10-31 19:21:13 +0700

    Added: Person class for tracking

---
+++ person.py
@@ -0,0 +1,22 @@
+import numpy;
+
+from utils import helpers;
+
+class Person:
+    def __init__(self, id, bbox: numpy.ndarray) -> None:
+        self.id = id;
+        self.pitch = 0.0;
+        self.yaw = 0.0;
+        self.bbox = bbox;
+
+    def update(self, bbox, pitch, yaw) -> None:
+        self.bbox = bbox;
+        self.pitch = pitch;
+        self.yaw = yaw;
+
+    def draw(self, frame) -> None:
+        helpers.draw_bbox(frame, self.bbox)
+
+        # TODO: draw with different colors
+        helpers.draw_gaze(frame, self.bbox, self.pitch, self.yaw)
+
