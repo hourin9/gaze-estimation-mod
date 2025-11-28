f3abe641c65b52988d654f687817694245b68d41
hourin <phucjaki@gmail.com>
2025-11-28 19:04:19 +0700

    Copied: pre_process(image)

--- server/main.py
+++ server/main.py
@@ -1,7 +1,31 @@
 from flask import Flask, Response;
 import cv2;
+import torch;
+from torchvision import transforms;
+import uniface;
+import numpy as np;
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu");
+idx_tensor = torch.arange(params.bins, device=device, dtype=torch.float32);
+face_detector = uniface.RetinaFace();
 
 app = Flask(__name__);
+
+def pre_process(image):
+    if image is None or image.size == 0:
+        image = np.full((448, 448, 3), 255, dtype=np.uint8);
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize(448),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    image = transform(image)
+    image_batch = image.unsqueeze(0)
+    return image_batch
 
 cam = cv2.VideoCapture(0);
 def get_webcam_frame():
